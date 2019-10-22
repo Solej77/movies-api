56b329802748ab41d2ceb088d09c4a05141d573c
Solej77 <ing.josgalo@gmail.com>
2019-10-21 21:53:10 -0500

    CRUD Express all

--- routes/movies.js
+++ routes/movies.js
@@ -17,6 +17,58 @@ function moviesApi(app) {
       next(error);
     }
   });
+
+  router.get('/:movieId', async function(req, res, next) {
+    try {
+      const movies = await Promise.resolve(moviesMock[0]);
+      res.status(200).json({
+        data: movies,
+        message: 'movie retrieved'
+      });
+      
+    } catch (error) {
+      next(error);
+    }
+  });
+
+  router.post('/', async function(req, res, next) {
+    try {
+      const createMovieId = await Promise.resolve(moviesMock[0].id);
+      res.status(201).json({
+        data: createMovieId,
+        message: 'movies created'
+      });
+      
+    } catch (error) {
+      next(error);
+    }
+  });
+
+  router.put('/:movieId', async function(req, res, next) {
+    try {
+      const updatedMoevieId = await Promise.resolve(moviesMock[0].id);
+      res.status(200).json({
+        data: updatedMoevieId,
+        message: 'movies updated'
+      });
+      
+    } catch (error) {
+      next(error);
+    }
+  });
+
+  router.delete('/:movieId', async function(req, res, next) {
+    try {
+      const deletedMoevieId = await Promise.resolve(moviesMock[0].id);
+      res.status(200).json({
+        data: deletedMoevieId,
+        message: 'movies deleted'
+      });
+      
+    } catch (error) {
+      next(error);
+    }
+  });
 }
 
 module.exports = moviesApi;
