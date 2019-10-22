0a490a4d2f3f6918c332b41fc128f40216fa8255
Solej77 <ing.josgalo@gmail.com>
2019-10-21 21:22:15 -0500

    CRUD on Express

--- index.js
+++ index.js
@@ -2,14 +2,9 @@ const express = require('express');
 const app = express();
 
 const { config } = require('./config/index');
+const moviesApi = require('./routes/movies.js');
 
-app.get('/', function(req, res) {
-  res.send('Hello Node + Express.js')
-});
-
-app.get('/json', function(req, res) {
-  res.json({hello: 'Node + Express.js'})
-});
+moviesApi(app);
 
 app.listen(config.port, function() {
   console.log(`Listening http://localhost:${config.port}`);
---
+++ utils/mocks/movies.js
@@ -0,0 +1,137 @@
+const moviesMock = [{
+  "id": "3ab698a7-0fe7-44d3-b9d9-3f9e4661dc5c",
+  "tittle": "Ip Man",
+  "year": 2001,
+  "cover": "http://dummyimage.com/235x171.png/5fa2dd/ffffff",
+  "description": "Curabitur at ipsum ac tellus semper interdum. Mauris ullamcorper purus sit amet nulla. Quisque arcu libero, rutrum ac, lobortis vel, dapibus at, diam.",
+  "duration": 2012,
+  "contentRating": "PG-13",
+  "source": "https://mapy.cz/imperdiet/sapien/urna.js",
+  "tags": [
+    "Crime|Drama",
+    "Comedy|Romance",
+    "Action|Adventure|Western",
+    "Horror|Mystery|Thriller",
+    "Drama"
+  ]
+}, {
+  "id": "2eb9abef-d61b-478d-ad03-56ad78dd744a",
+  "tittle": "Speedway Junky",
+  "year": 2011,
+  "cover": "http://dummyimage.com/157x199.bmp/5fa2dd/ffffff",
+  "description": "Proin interdum mauris non ligula pellentesque ultrices. Phasellus id sapien in sapien iaculis congue. Vivamus metus arcu, adipiscing molestie, hendrerit at, vulputate vitae, nisl.\n\nAenean lectus. Pellentesque eget nunc. Donec quis orci eget orci vehicula condimentum.",
+  "duration": 2026,
+  "contentRating": "PG-13",
+  "source": "http://ibm.com/tristique.jsp",
+  "tags": [
+    "Action|Drama|War",
+    "Drama",
+    "Thriller",
+    "Comedy|Mystery"
+  ]
+}, {
+  "id": "ede8a101-37a8-4e21-952c-3f14a304f57d",
+  "tittle": "Camille Claudel 1915",
+  "year": 2011,
+  "cover": "http://dummyimage.com/232x128.bmp/ff4444/ffffff",
+  "description": "Integer ac leo. Pellentesque ultrices mattis odio. Donec vitae nisi.\n\nNam ultrices, libero non mattis pulvinar, nulla pede ullamcorper augue, a suscipit nulla elit ac nulla. Sed vel enim sit amet nunc viverra dapibus. Nulla suscipit ligula in lacus.\n\nCurabitur at ipsum ac tellus semper interdum. Mauris ullamcorper purus sit amet nulla. Quisque arcu libero, rutrum ac, lobortis vel, dapibus at, diam.",
+  "duration": 1910,
+  "contentRating": "G",
+  "source": "https://slideshare.net/pede/ac/diam/cras.png",
+  "tags": [
+    "Crime|Drama|Mystery|Thriller"
+  ]
+}, {
+  "id": "c5437a77-c322-4515-84f3-6d7b2d46ad60",
+  "tittle": "Scarface",
+  "year": 2004,
+  "cover": "http://dummyimage.com/197x131.bmp/ff4444/ffffff",
+  "description": "Praesent blandit. Nam nulla. Integer pede justo, lacinia eget, tincidunt eget, tempus vel, pede.",
+  "duration": 1996,
+  "contentRating": "PG-13",
+  "source": "http://amazon.co.jp/quam/pharetra/magna/ac/consequat.js",
+  "tags": [
+    "Crime|Thriller"
+  ]
+}, {
+  "id": "3a6097dc-5891-4deb-bf0f-5ebeac1f1f86",
+  "tittle": "Red Desert (Deserto rosso, Il)",
+  "year": 2008,
+  "cover": "http://dummyimage.com/189x107.jpg/ff4444/ffffff",
+  "description": "Maecenas leo odio, condimentum id, luctus nec, molestie sed, justo. Pellentesque viverra pede ac diam. Cras pellentesque volutpat dui.\n\nMaecenas tristique, est et tempus semper, est quam pharetra magna, ac consequat metus sapien ut nunc. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Mauris viverra diam vitae quam. Suspendisse potenti.",
+  "duration": 1968,
+  "contentRating": "R",
+  "source": "https://dailymail.co.uk/euismod/scelerisque.js",
+  "tags": [
+    "Drama"
+  ]
+}, {
+  "id": "edd75632-9d1b-42b0-9e04-2037484c3961",
+  "tittle": "Michael Shayne: Private Detective",
+  "year": 1979,
+  "cover": "http://dummyimage.com/222x141.png/cc0000/ffffff",
+  "description": "Quisque porta volutpat erat. Quisque erat eros, viverra eget, congue eget, semper rutrum, nulla. Nunc purus.",
+  "duration": 2018,
+  "contentRating": "PG-13",
+  "source": "http://360.cn/lacus/morbi.jsp",
+  "tags": [
+    "Drama"
+  ]
+}, {
+  "id": "f4fcfa4d-4679-4ca3-8cc8-0c98441e0617",
+  "tittle": "Some Like It Hot",
+  "year": 1992,
+  "cover": "http://dummyimage.com/100x203.bmp/ff4444/ffffff",
+  "description": "Etiam vel augue. Vestibulum rutrum rutrum neque. Aenean auctor gravida sem.\n\nPraesent id massa id nisl venenatis lacinia. Aenean sit amet justo. Morbi ut odio.\n\nCras mi pede, malesuada in, imperdiet et, commodo vulputate, justo. In blandit ultrices enim. Lorem ipsum dolor sit amet, consectetuer adipiscing elit.",
+  "duration": 1913,
+  "contentRating": "NC-17",
+  "source": "https://state.tx.us/risus/semper/porta.json",
+  "tags": [
+    "Crime|Drama"
+  ]
+}, {
+  "id": "024ed876-9879-468d-a724-302b15e6f2e6",
+  "tittle": "Frosty the Snowman",
+  "year": 1987,
+  "cover": "http://dummyimage.com/102x164.bmp/cc0000/ffffff",
+  "description": "Cras mi pede, malesuada in, imperdiet et, commodo vulputate, justo. In blandit ultrices enim. Lorem ipsum dolor sit amet, consectetuer adipiscing elit.\n\nProin interdum mauris non ligula pellentesque ultrices. Phasellus id sapien in sapien iaculis congue. Vivamus metus arcu, adipiscing molestie, hendrerit at, vulputate vitae, nisl.\n\nAenean lectus. Pellentesque eget nunc. Donec quis orci eget orci vehicula condimentum.",
+  "duration": 2022,
+  "contentRating": "NC-17",
+  "source": "http://wikispaces.com/in.html",
+  "tags": [
+    "Drama",
+    "Drama|Horror|Mystery|Thriller",
+    "Drama"
+  ]
+}, {
+  "id": "2b7b5b45-3752-4f1c-9eea-cf8bb742ce59",
+  "tittle": "Glitter",
+  "year": 2006,
+  "cover": "http://dummyimage.com/141x168.png/5fa2dd/ffffff",
+  "description": "Nam ultrices, libero non mattis pulvinar, nulla pede ullamcorper augue, a suscipit nulla elit ac nulla. Sed vel enim sit amet nunc viverra dapibus. Nulla suscipit ligula in lacus.\n\nCurabitur at ipsum ac tellus semper interdum. Mauris ullamcorper purus sit amet nulla. Quisque arcu libero, rutrum ac, lobortis vel, dapibus at, diam.",
+  "duration": 1926,
+  "contentRating": "G",
+  "source": "https://dropbox.com/consectetuer/adipiscing/elit/proin/interdum/mauris.xml",
+  "tags": [
+    "Action|Children|Comedy",
+    "Action|Thriller",
+    "Comedy|Drama",
+    "Action|Adventure|Children"
+  ]
+}, {
+  "id": "30f3fa8c-8fc1-400e-9222-78eaecf27042",
+  "tittle": "The Overnight",
+  "year": 1979,
+  "cover": "http://dummyimage.com/153x149.bmp/5fa2dd/ffffff",
+  "description": "Etiam vel augue. Vestibulum rutrum rutrum neque. Aenean auctor gravida sem.\n\nPraesent id massa id nisl venenatis lacinia. Aenean sit amet justo. Morbi ut odio.",
+  "duration": 2006,
+  "contentRating": "NC-17",
+  "source": "http://mozilla.com/ante/vivamus/tortor/duis.png",
+  "tags": [
+    "Action|Adventure|Sci-Fi"
+  ]
+}];
+
+module.exports = {
+  moviesMock
+};
