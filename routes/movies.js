7fe731f4e34996378200bfc67fdfdd898e1958b1
Solej77 <ing.josgalo@gmail.com>
2019-10-22 08:48:57 -0500

    Creating service layer in Express

--- routes/movies.js
+++ routes/movies.js
@@ -1,13 +1,26 @@
+/**
+ * La unica responsabilidad de las rutas es como recibir los parametros,
+ * para que posteriormente puedan ser enviados a los servicios
+ */
+
 const express = require('express');
-const { moviesMock } = require('../utils/mocks/movies');
+// importar la capa de servicios
+const MoviesService = require('../services/movies');
+
 
 function moviesApi(app) {
   const router = express.Router();
   app.use('/api/movies', router);
 
+  // Instanciamos los servicios
+  const moviesService = new MoviesService();
+
   router.get('/', async function(req, res, next) {
+    //Se obtiene de la lestura de la Request y Response Object
+    const { tags } = req.query;
+
     try {
-      const movies = await Promise.resolve(moviesMock);
+      const movies = await moviesService.getMovies({ tags });
       res.status(200).json({
         data: movies,
         message: 'movies listed'
@@ -19,8 +32,18 @@ function moviesApi(app) {
   });
 
   router.get('/:movieId', async function(req, res, next) {
+    
+    /**
+     * ¿En que casos utilxar req.query y req.params?
+     * 
+     * req.query = es cuando utilizamos el signo de ? nombre de query y se puede concatenar
+     * 
+     * req.params = es cuando los datos estan establecidosos en la url
+     */
+    const { movieId } = req.params;
+
     try {
-      const movies = await Promise.resolve(moviesMock[0]);
+      const movies = await moviesService.getMovie({ movieId })
       res.status(200).json({
         data: movies,
         message: 'movie retrieved'
@@ -32,8 +55,11 @@ function moviesApi(app) {
   });
 
   router.post('/', async function(req, res, next) {
+
+    const { body: movie } = req;
+
     try {
-      const createMovieId = await Promise.resolve(moviesMock[0].id);
+      const createMovieId = await moviesService.createMovie({ movie });
       res.status(201).json({
         data: createMovieId,
         message: 'movies created'
@@ -45,8 +71,14 @@ function moviesApi(app) {
   });
 
   router.put('/:movieId', async function(req, res, next) {
+  const { movieId } = req.params;
+  const { body: movie } = req;
+
     try {
-      const updatedMoevieId = await Promise.resolve(moviesMock[0].id);
+      const updatedMoevieId = await moviesService.updateMovie({
+        movieId, 
+        movie
+      });
       res.status(200).json({
         data: updatedMoevieId,
         message: 'movies updated'
@@ -58,8 +90,9 @@ function moviesApi(app) {
   });
 
   router.delete('/:movieId', async function(req, res, next) {
+    const { movieId } = req.params;
     try {
-      const deletedMoevieId = await Promise.resolve(moviesMock[0].id);
+      const deletedMoevieId = await moviesService.deleteMovie({ movieId });
       res.status(200).json({
         data: deletedMoevieId,
         message: 'movies deleted'
