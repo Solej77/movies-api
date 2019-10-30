f329e769b3fb4836f0449230d57bd19ce61c5f32
Solej77 <ing.josgalo@gmail.com>
2019-10-30 12:51:23 -0600

    Debugging e inspect

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express();
+const debug = require('debug')('app:server');
 
 const { config } = require('./config/index');
 const moviesApi = require('./routes/movies.js');
@@ -27,5 +28,5 @@ app.use(errorHandler);
 
 app.listen(config.port, function() {
   // eslint-disable-next-line no-console
-  console.log(`Listening http://localhost:${config.port}`);
+  debug(`Listening http://localhost:${config.port}`);
 });
